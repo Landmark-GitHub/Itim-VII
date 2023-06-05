e96d4e40122f27c1deb69aaec095d5676592f322
landmark_github <l.landmark.mail@gmail.com>
2023-06-05 23:24:46 +0700

    Moblie Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,17 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="bg-red-300 flex flex-col items-center justify-center h-screen p-2">
-      <div className="bg-white w-2/4 h-2/4 text-center text-3xl font-bold rounded-lg shadow-x p-4">
-        <div className="h-1/4 text-center text-3xl">
-          <h1 className="text-5xl font-bold">Home</h1>
+    <div className="bg-red-300 flex flex-col items-center justify-center min-h-screen p-4">
+      <div className="bg-white w-11/12 md:w-3/4 lg:w-2/4 xl:w-1/3 text-center text-3xl font-bold rounded-lg shadow-lg p-8">
+        <div className="h-1/4 text-center">
+          <h1 className="text-4xl md:text-5xl font-bold">Home</h1>
         </div>
         <div className="h-3/4 flex flex-col items-center justify-center">
-          <Link className="my-2 w-3/4 bg-gray-200 text-2xl font-bold rounded-lg shadow-xl hover:bg-lime-600 transition-colors duration-300 flex items-center justify-center h-10" 
+          <Link className="my-4 w-full md:w-3/4 bg-gray-200 text-2xl font-bold rounded-lg shadow-xl hover:bg-lime-600 transition-colors duration-300 flex items-center justify-center h-12" 
           href="/select">
             Save Requisition
           </Link>
-          <Link className="my-2 w-3/4 bg-gray-200 text-2xl font-bold rounded-lg shadow-xl hover:bg-pink-400 transition-colors duration-300 flex items-center justify-center h-10" 
+          <Link className="my-4 w-full md:w-3/4 bg-gray-200 text-2xl font-bold rounded-lg shadow-xl hover:bg-pink-400 transition-colors duration-300 flex items-center justify-center h-12"
           href="/">
             Settings
           </Link>
