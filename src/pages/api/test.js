13fd019146286fef1ce74aef85a2b509cfabf96e
landmark_github <l.landmark.mail@gmail.com>
2023-06-07 15:17:27 +0700

    Update test.js

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -3,17 +3,17 @@ const mysql = require('mysql2');
 // create the connection to the database
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-export default function handler(req, res) {
-  // simple query
-  connection.query(
-    'SELECT * FROM `itim`',
-    function (err, results, fields) {
-        res.status(200).json(results);
-    }
-  );
-}
-
-
 // export default function handler(req, res) {
-//     res.status(200).json({ name: 'John Doe' });
+//   // simple query
+//   connection.query(
+//     'SELECT * FROM `itim`',
+//     function (err, results, fields) {
+//         res.status(200).json(results);
+//     }
+//   );
 // }
+
+
+export default function handler(req, res) {
+    res.status(200).json({ name: 'John Doe' });
+}
