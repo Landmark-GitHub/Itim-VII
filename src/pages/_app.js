fc234d0cfee9bfd4d48f255a341bfc0a6db568dc
landmark_github <l.landmark.mail@gmail.com>
2023-06-07 16:38:33 +0700

    Analasy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import Layout from './components/LayoutTest'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return (<Component {...pageProps} />)
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
