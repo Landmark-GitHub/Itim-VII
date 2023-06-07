3ced80d39b3374f60790ee4a896d4441087d6a79
landmark_github <l.landmark.mail@gmail.com>
2023-06-07 16:35:38 +0700

    gggg

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,20 +1,5 @@
 const mysql = require('mysql2');
 
-// create the connection to the database
-// const connection = mysql.createConnection(process.env.DATABASE_URL);
-
-
-// export default function handler(req, res) {
-//   // simple query
-//   connection.query(
-//     'SELECT * FROM `itim`',
-//     function (err, results, fields) {
-//         res.status(200).json(results);
-//     }
-//   );
-// }
-
-
 export default function handler(req, res) {
     const dream_itim = mysql.createPool(process.env.DATABASE_URL)
     if (req.method === 'GET') {
@@ -24,6 +9,7 @@ export default function handler(req, res) {
                 res.status(200).json(results);
             }
         );
+    } else {
+        res.status(200).json({ name: 'John Do Hee' });
     }
-    res.status(200).json({ name: 'John Do Hee' });
 }
