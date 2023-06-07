02055ad22f3e9f50bd5e35a530f7b2dd0d405304
landmark_github <l.landmark.mail@gmail.com>
2023-06-07 15:06:36 +0700

    API test

--- src/pages/api/itim.js
+++ src/pages/api/itim.js
@@ -4,16 +4,16 @@ const mysql = require('mysql2');
 // create the connection to the database
 const connection = mysql.createPool(process.env.DATABASE_URL);
 
-export default function itimdb(req, res) {
+export default function itimdb(NextApiRequest, NextApiResponse) {
   // simple query
   connection.query(
     'SELECT * FROM `itim`',
     function (err, results, fields) {
-      res.status(200).json(results);
+      NextApiResponse.status(200).json(results);
     }
   );
 
   if (connection.status === 500) {
-    res.send('Hello');
+    NextApiResponse.send('Hello');
   }
 }
--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,3 +1,23 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' });
+const mysql = require('mysql2');
+
+// create the connection to the database
+const connection = mysql.createPool(process.env.DATABASE_URL);
+
+export default function itim(req, res) {
+  // simple query
+  connection.query(
+    'SELECT * FROM `itim`',
+    function (err, results, fields) {
+      NextApiResponse.status(200).json(results);
+    }
+  );
+
+//   if (connection.status === 500) {
+//     NextApiResponse.send('Hello');
+//   }
 }
+
+
+// export default function handler(req, res) {
+//     res.status(200).json({ name: 'John Doe' });
+// }
