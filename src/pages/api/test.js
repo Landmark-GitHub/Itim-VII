8776594e2fc8f5bc4786b1abc4eb8a334f017812
landmark_github <l.landmark.mail@gmail.com>
2023-06-07 15:58:35 +0700

    ddddd

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 // create the connection to the database
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+// const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 // export default function handler(req, res) {
 //   // simple query
