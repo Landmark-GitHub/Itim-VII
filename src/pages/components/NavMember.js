719b876b5ad9bd16fea9e74087db1e368b81aae7
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 17:13:07 +0700

    Update NavMember.js

--- src/pages/components/NavMember.js
+++ src/pages/components/NavMember.js
@@ -131,7 +131,7 @@ const NavMember = () => {
     console.log(newMember)
 
     try {
-      const response = await axios.post('https://important-shrug-bee.cyclic.app/postMembers', {mode:'cors'} ,newMember)
+      const response = await axios.post('https://important-shrug-bee.cyclic.app/postMembers',newMember)
       axiosMember();
       setModalAdd(!modalAdd);
     } catch (error) {
