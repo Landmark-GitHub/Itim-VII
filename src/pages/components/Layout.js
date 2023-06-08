8d1ffb9bacbea459b93c74f31bff23f85cc44f92
landmark_github <l.landmark.mail@gmail.com>
2023-06-08 12:35:54 +0700

    Update Layout.js

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -19,7 +19,7 @@ export default function Layout({ children, ...props }) {
 
     return (
         <>
-            <main className='flex h-screen w-full'>
+            <main className='flex w-full'>
                 <div div="true" className={`${openMenu ? 'w-3/12' : 'w-0 opacity-0 translate-x-(-28)'} overflow-hidden duration-300`}>
                     <NavMember
                     />
@@ -29,7 +29,7 @@ export default function Layout({ children, ...props }) {
                     <Header
                     >
                     </Header>
-                    <div className='h-5/6 overflow-y-scroll'>
+                    <div className='h-5/6'>
                         {children}
                     </div>
                     <button onClick={toggleMenu} className={`absolute bottom-2 ${openMenu ? 'left-3/12' : 'left-2 '} duration-300 z-10 px-4 py-2 bg-gray-500 text-white rounded`}>
